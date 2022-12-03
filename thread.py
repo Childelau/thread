3e0712c1e3795076ae734c3b7953530cd9150b04
liuwei <1299337429@qq.com>
2022-12-03 10:30:52 +0800

    init

---
+++ thread.py
@@ -0,0 +1,12 @@
+import threading
+import time
+
+
+def printNumber(n: int) -> None:
+    while True:
+        print(n)
+        time.sleep(n)
+
+for i in range(1,3):
+    t = threading.Thread(target=printNumber, args=(i,))
+    t.start()
