3ddba3ed818dc411c8d975701f41667f83b804bb
liuwei <1299337429@qq.com>
2022-12-06 10:01:55 +0800

    init

--- thread.py
+++ thread.py
@@ -1,12 +1,14 @@
 import threading
-import time
 
+class MyThread(threading.Thread):
+    def __init__(self, thread_name):
+        # 注意：一定要显式的调用父类的初始化函数。
+        super(MyThread, self).__init__(name=thread_name)
 
-def printNumber(n: int) -> None:
-    while True:
-        print(n)
-        time.sleep(n)
+    def run(self):
+        print("%s正在运行中......" % self.name)
 
-for i in range(1,3):
-    t = threading.Thread(target=printNumber, args=(i,))
-    t.start()
+if __name__ == '__main__':    
+    for i in range(10):
+        MyThread("thread-" + str(i)).start()
+
